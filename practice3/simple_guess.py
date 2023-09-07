59a5d3094785cf66f9bf51165be4d974889399a3
Danila Zolotarev <danila-zol@list.ru>
2023-09-07 11:27:22 +0300

    Lecture 3

--- practice3/simple_guess.py
+++ practice3/simple_guess.py
@@ -2,17 +2,18 @@
 # Вывод корня, если он целый
 # Если дробный, вывести "трудно"
 # С помощью функции guess
-from math import floor
 
 def guess(root):
-    for n in range(root):
+    for n in range(root+1):
         if (n**2 == root):
             return n
     
     return None
 
 root = guess(int(input("Enter N: ")))
-if root:
-    print(root)
-else:
-    print("трудно")
+print(root if root else "трудно")
+
+# if root:
+#     print(root)
+# else:
+#     print("трудно")
