976fc632bc2acd4466c342474093fc5cd8c5596f
Danila Zolotarev <danila-zol@list.ru>
2023-09-08 11:21:47 +0300

    Racer practice

---
+++ lecture_4/exercise.py
@@ -0,0 +1,9 @@
+def greet():
+    y = "Bob"
+    print(f"Hello {y}")
+
+greet()
+
+if __name__ == "__main__":
+    x = 42
+    print(f"y in main is exists: {'y' in locals()}")
---
+++ practice4/race.py
@@ -0,0 +1,35 @@
+from import_this import RACE_DATA
+
+# Output Structure
+
+# Выиграл - VOVA!!! Поздравляем!!
+# _______________________________
+
+# Первые три места:
+
+# Гонщик на 1 месте:
+#     Имя: Vova
+#     Команда: KAMAZ
+#     Время: 01:00:00 (H:M:S)
+    
+# Гонщик на 2 месте:
+#     Имя: фыв
+#     Команда: ыфв
+#     Время: 02:00:00 (H:M:S)
+
+racers_arr = [None for x in RACE_DATA.keys()]
+for rd in RACE_DATA.values():
+    racers_arr[rd['FinishedPlace'] - 1] = rd
+
+win_message = f"Выиграл - {racers_arr[0]['RacerName']}!!! Поздравляем!!\n"
+print(win_message + "-" * len(win_message) + "\n")
+
+print("Первые три места:\n")
+for rd in racers_arr[:3]:
+    ts = rd["FinishedTimeSeconds"] % 60
+    tm = rd["FinishedTimeSeconds"] % 3600 // 60
+    th = rd["FinishedTimeSeconds"] // 3600
+    print(f"Гонщик на {racers_arr.index(rd) + 1} месте:\n\
+    \tИмя: {rd['RacerName']}\n\
+    \tКоманда: {rd['RacerTeam']}\n\
+    \tВремя: {th}:{tm}:{ts}\n")
